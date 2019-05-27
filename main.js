d3f52bed6127d3819c68c07df34d251a8be4ef06
GETYAMAME <pon.mikan10@gmail.com>
2019-05-28 02:36:07 +0900

    1章と2章の半分まで

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
-document.addEventListener('DOMContentLoaded',function(){
-  window.app = new Vue()
-})
 var app = new Vue({
     el: '#app',
     data: {
-      message:'hello Vue.js!'
+      list:['りんご','ばなな','いちご'],
+      message:'めっせ',
+      show: true,
+      val: '1234',
+      classObject: {
+        child: true,
+        'is-active': true
+      },
+      styleObject: {
+        color: 'red',
+        backgroundColor: 'lightgray'
+      },
+      item: {
+        id: 1,
+        src: 'item1.jpg',
+        alt: '商品１サムネイル',
+        width: 200,
+        height: 200
+      },
+      radius:50,
+    },
+    methods: {
+      handleClick: function(event){
+        app.show = !app.show
+      }
     }
 })
+app.list.push('オレンジ')
 console.log(app.message)
